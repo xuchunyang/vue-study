ba8ae93187e73f8d22edb840091e57ab4197473d
Xu Chunyang <xuchunyang56@gmail.com>
2021-03-20 20:21:14 +0800

    18-single-file-vue: Hello, .vue

---
+++ 18-single-file-vue/src/components/GoodBye.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>GoodBye {{ who || "World" }}</h1>
+</template>
+
+<script>
+export default {
+  name: "GoodBye",
+  props: {
+    who: String,
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: blueviolet;
+}
+</style>
